7c8ed1eaf4e9a9989cc3cdd1e36bf8ccac67e8c4
Miha Dimec <miha.dimec@visionect.com>
2018-07-26 16:49:22 +0200

    cgo: working example

--- cgo.go
+++ cgo.go
@@ -1,25 +1,13 @@
 package main
 
-//// this C code could also have been in external *.c file,
-//// or linked as pre-compiled library
-// #include <stdio.h>
+//// this C code could also have been in external *.c file, or linked as pre-compiled library
 // #include <stdlib.h>
 // #include <string.h>
 //
-// void hello(void)
-// {
-//   printf("hello from C\n");
-// }
-//
 // char* join_strings(const char *s1, const char *s2)
 // {
-//   char* result;
-//
-//   result = malloc(strlen(s1) + strlen(s2) + 1);
-//   if(result == NULL)
-//     return NULL;
-//   strcpy(result, s1);
-//   strcat(result, s2);
+//   char* result = malloc(strlen(s1) + strlen(s2) + 1); // BUG check if malloc returns NULL
+//   strcat(strcpy(result, s1), s2);
 //   return result;
 // }
 import "C"
@@ -29,11 +17,6 @@ import (
 )
 
 func main() {
-	C.hello()
-	// it seams that Go presentation code runner "present",
-	// prints nothing from <stdio.h> without `fflush()`
-	C.fflush(C.stdout)
-
 	joinedC := C.join_strings(C.CString("Go "), C.CString("language"))
 	joined := C.GoString(joinedC)
 	C.free(unsafe.Pointer(joinedC))
