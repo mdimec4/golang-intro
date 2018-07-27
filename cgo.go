f9a2bed4c342aabc93baf27dfa4e95aa46e3efdf
Miha Dimec <miha.dimec@visionect.com>
2018-07-27 10:31:06 +0200

    fix cgo example to not leak memory

--- cgo.go
+++ cgo.go
@@ -4,10 +4,9 @@ package main
 // #include <stdlib.h>
 // #include <string.h>
 //
-// char* join_strings(const char *s1, const char *s2)
-// {
-//   char* result = malloc(strlen(s1) + strlen(s2) + 1); // BUG check if malloc returns NULL
-//   return strcat(strcpy(result, s1), s2);
+// char* join_strings(const char *s1, const char *s2)  {
+//   char* joined = malloc(strlen(s1) + strlen(s2) + 1); // BUG check if malloc returns NULL
+//   return strcat(strcpy(joined, s1), s2);
 // }
 import "C"
 import (
@@ -16,8 +15,10 @@ import (
 )
 
 func main() {
-	joinedC := C.join_strings(C.CString("Go "), C.CString("language"))
-	joined := C.GoString(joinedC)
-	C.free(unsafe.Pointer(joinedC))
-	fmt.Println("C function 'join_string()' returned: ", joined)
+	c_s1, c_s2 := C.CString("Go "), C.CString("language")
+	c_joined := C.join_strings(c_s1, c_s2)
+	C.free(unsafe.Pointer(c_s1))
+	C.free(unsafe.Pointer(c_s2))
+	fmt.Println("C function 'join_string()' returned: ", C.GoString(c_joined))
+	C.free(unsafe.Pointer(c_joined))
 }
