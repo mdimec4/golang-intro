de65ac28f0d8e0216e9649a8fbd206b01988e86e
Miha Dimec <miha.dimec@visionect.com>
2018-07-27 11:56:01 +0200

    redesign interfaces example

--- interfaces.go
+++ interfaces.go
@@ -5,10 +5,14 @@ import (
 	"math"
 )
 
-// START OMIT
+// START ABSER OMIT
 type Abser interface { // Interface definition
 	Abs() float64 // requires Abs() method with this signature
 }
+
+//END ABSER OMIT
+
+// START VERTEX OMIT
 
 type Vertex struct {
 	X, Y float64
@@ -18,17 +22,29 @@ func (v Vertex) Abs() float64 { // Vertex implemnts Abser
 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 }
 
+// END VERTEX OMIT
+
+// START MYFLOAT OMIT
 type MyFloat float64
 
 func (f MyFloat) Abs() float64 { // myFloat also implements Abser
 	return math.Abs(float64(f))
 }
 
+// END MYFLOAT OMIT
+
+// START MAIN OMIT
+// PrintAbs accepts anything that implements Abser as a parameter
+func PrintAbs(v Abser) {
+	a := v.Abs()
+	fmt.Println(a)
+}
+
 func main() {
 	v := []Abser{Vertex{3, 4}, MyFloat(-3.14)}
 	for _, a := range v {
-		fmt.Println(a.Abs())
+		PrintAbs(a)
 	}
 }
 
-//END OMIT
+// END MAIN OMIT
